ae3256ed0287701822ed953acf310d089f70396d
prajkta Praygude <praktapaygude@example.com>
2026-06-24 16:53:05 +0530

    email varification

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -64,7 +64,7 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,
-  secure: process.env.EMAIL_PORT == 465, // true for 465, false for other ports
+  secure: process.env.EMAIL_PORT == 587, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
